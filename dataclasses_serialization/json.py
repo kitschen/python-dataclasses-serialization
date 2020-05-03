d4464b3cc55e5e3d1c531805dd8e4fca208eb254
philipp.buluschek <philipp.buluschek@domo-safety.com>
2020-05-03 07:11:33 +0200

    Using ordered dicts so that (de-)serializer order selection is predictable

--- dataclasses_serialization/json.py
+++ dataclasses_serialization/json.py
@@ -1,4 +1,5 @@
 import json
+from collections import OrderedDict
 
 from dataclasses_serialization.serializer_base import noop_serialization, noop_deserialization, dict_serialization, dict_deserialization, list_deserialization, Serializer
 
@@ -10,16 +11,18 @@ __all__ = [
 ]
 
 JSONSerializer = Serializer(
-    serialization_functions={
-        dict: lambda dct: dict_serialization(dct, key_serialization_func=JSONSerializer.serialize, value_serialization_func=JSONSerializer.serialize),
-        list: lambda lst: list(map(JSONSerializer.serialize, lst)),
-        (str, int, float, bool, type(None)): noop_serialization
-    },
-    deserialization_functions={
-        dict: lambda cls, dct: dict_deserialization(cls, dct, key_deserialization_func=JSONSerializer.deserialize, value_deserialization_func=JSONSerializer.deserialize),
-        list: lambda cls, lst: list_deserialization(cls, lst, deserialization_func=JSONSerializer.deserialize),
-        (str, int, float, bool, type(None)): noop_deserialization
-    }
+    serialization_functions=OrderedDict([
+        (dict, lambda dct: dict_serialization(dct, key_serialization_func=JSONSerializer.serialize,
+                                              value_serialization_func=JSONSerializer.serialize)),
+        (list, lambda lst: list(map(JSONSerializer.serialize, lst))),
+        ((str, int, float, bool, type(None)), noop_serialization)
+    ]),
+    deserialization_functions=OrderedDict([
+        (dict, lambda cls, dct: dict_deserialization(cls, dct, key_deserialization_func=JSONSerializer.deserialize,
+                                                     value_deserialization_func=JSONSerializer.deserialize)),
+        (list, lambda cls, lst: list_deserialization(cls, lst, deserialization_func=JSONSerializer.deserialize)),
+        ((str, int, float, bool, type(None)), noop_deserialization)
+    ])
 )
 
 
